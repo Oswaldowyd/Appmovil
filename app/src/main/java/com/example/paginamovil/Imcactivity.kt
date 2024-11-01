2388f4f05d1c48e88244d4b3e25cc83df62aef9b
Oswaldo Sevilla Vera <sevillaveraoswaldo@gmail.com>
2024-11-01 00:35:44 -0700

    sgundo commit

--- app/src/main/java/com/example/paginamovil/Imcactivity.kt
+++ app/src/main/java/com/example/paginamovil/Imcactivity.kt
@@ -1,9 +1,10 @@
-package com.example.paginamovil
+package com.example.myapplication
 
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -11,17 +12,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Imcactivity : AppCompatActivity() {
-    private lateinit var txtAltura : EditText
-    private lateinit var txtPeso : EditText
-    private lateinit var txtResultado : TextView
-    private lateinit var btnLimpiar : Button
-    private lateinit var btnCerrar : Button
-    private lateinit var btnCalcular : Button
+class grados : AppCompatActivity() {
+    private lateinit var txtCantidad : EditText
+    private lateinit var  txtResultado: TextView
+    private lateinit var  rbdCel: RadioButton
+    private lateinit var  rbdFar: RadioButton
+
+    private lateinit var  btnCalcular: Button
+    private lateinit var  btnLimpiar: Button
+    private lateinit var  btnCerrar: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_imcactivity)
+        setContentView(R.layout.activity_grados)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -29,32 +34,47 @@ class Imcactivity : AppCompatActivity() {
         }
     }
 
-    fun iniciarComponentes(){
-        txtAltura = findViewById(R.id.txtAltura) as EditText
-        txtPeso = findViewById(R.id.txtPeso)
-        txtResultado = findViewById(R.id.txtResultado) as TextView
-
-        btnCerrar = findViewById(R.id.btnCerrar) as Button
-        btnLimpiar = findViewById(R.id.btnLimpiar) as Button
-        btnCalcular = findViewById(R.id.btnCalcular) as Button
+    public fun iniciarComponentes(){
+        txtCantidad=findViewById(R.id.txtCantidad) as EditText
+        txtResultado=findViewById(R.id.txtResultado as TextView
+                rbdCel=findViewById(R.id.rbdCel) as RadioButton
+                rbdFar=findViewById(R.id.rbdFar) as RadioButton
+                btnCalcular=findViewById(R.id.btnCalcular) as Button
+                btnLimpiar=findViewById(R.id.btnLimpiar) as Button
+                btnCerrar=findViewById(R.id.btnCerrar) as Button
     }
 
-    fun eventosClic(){
-        btnCalcular.setOnClickListener(View.OnClickListener {
-            //validar
-
-
-
-
-
-            if(txtPeso.text.toString().contentEquals(charSequence = "")
-                !! txtAltura.text.toString().contentEquals(charSequence = ""))
+    public fun eventosClick(){
+        btnCalcular.setOnClickListener(View.OnClickListener){
+            if (txtCantidad.text.toString().contentEquals(charSequence = )){
+                Toast.makeText(context: this,text: "falto capturar entidad", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                var cantidad : Float = txtCantidad.text.toString().toFloat()
+                if (rbdCel.isChecked){
+                    var celcius : Float = 0.0f
+                    celcius =( cantidad*9/5)+32;
+                    txtResultado.text = celcius.toString()
+                }
+                if (rbdCel.isChecked){
+                    var celcius : Float = 0.0f
+                    celcius =( cantidad 32)*5/9;
+                    txtResultado.text = Fahrenheint.toString()
+                }
+            })
         }
-    }
-
-    fun eventosClic(){
-        btnCalcular.setOnClickListener(View.OnClickListener {
-            Toast.makeText()
+        btnLimpiar.setOnClickListener {
+            txtResultado.text = ""
+            txtCantidad.text.clear()
+            txtResultado.text.clear()
+        }
+        btnCerrar.setOnClickListener(View.onClickListener{
+            finish()
         })
+
+
+
+
+
+
     }
-}
--- app/src/main/java/com/example/paginamovil/Menuactivity.kt
+++ app/src/main/java/com/example/paginamovil/Menuactivity.kt
@@ -1,20 +1,45 @@
 package com.example.paginamovil
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Telephony.Mms.Intents
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Menuactivity : AppCompatActivity() {
+    private lateinit var crvHola :CardView
+    private lateinit var crvImc :CardView
+    private lateinit var crvGrados :CardView
+    private lateinit var crvMoneda :CardView
+    private lateinit var crvCotizacion :CardView
+    private lateinit var crvSalir :CardView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_menuactivity)
+        iniciarComponentes()
+        eventosClic()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun iniciarComponentes(){
+        crvHola = findViewById(R.id.crvHola) as CardView
+        crvImc = findViewById(R.id.crvImc) as CardView
+        crvGrados = findViewById(R.id.crvConvertir) as CardView
+        crvMoneda = findViewById(R.id.crvMoneda) as CardView
+        crvCotizacion = findViewById(R.id.crvCotizacion) as CardView
+        crvSalir = findViewById(R.id.crvSalir) as CardView
+    }
+    fun eventosClic(){
+        crvHola.setOnClickListener(View.OnClickListener {
+            val intent = Intent(packageName:this,MainActivity::class.java)
+       startActivity(intent) })
     }
 }
